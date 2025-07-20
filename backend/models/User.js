72a272e442170f6555d46cf33499e1619ae65aec
shdowshogan <brucebanner0208@gmail.com>
2025-07-20 17:46:05 +0530

    data validation add

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose
 
 const UserSchema = new Schema({
   name: {
@@ -8,7 +9,7 @@ const UserSchema = new Schema({
   email: {
     type:String,
     required:true,
-    unique:true
+    // unique:true
   }, 
   password: {
     type:String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.get("/", (req, res) => {
-  const obj = {
-    a: "this",
-    number: 28,
-  };
-  res.json(obj);
+router.post("/",[
+    body('name','Enter a valid name').isLength({min:3}), 
+    body('email','enter a valid email').isEmail(), 
+    body('password','Paddword must atleast 8 characters').isLength({min:8}), 
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+  res.send(req.body)
 });
 
 module.exports = router;
