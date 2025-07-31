06eb06cfc58f2e0fd78dd31ca43d1d585497af77
shdowshogan <brucebanner0208@gmail.com>
2025-07-31 18:39:48 +0530

    added login signup buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,15 +34,8 @@ const Navbar = () => {
               </li>
             </ul>
             <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
+              <Link className="btn login-btn mx-2" to='/login'  role="button">Login</Link>
+              <Link className="btn signup-btn mx-2"  to='/signup' role="button">Sign Up</Link>
             </form>
           </div>
         </div>
